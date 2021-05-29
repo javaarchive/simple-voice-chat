f7b2eb9885ded8334dd986d60b102fbbfa693548
Raymond <awesomecatstudio+tojavaarchive@gmail.com>
2021-05-29 01:29:10 +0000

    fix git

---
+++ fixgit.sh
@@ -0,0 +1,3 @@
+git config --global user.name "Raymond"
+git config --global user.email awesomecatstudio+tojavaarchive@gmail.com
+git rebase -i 7074e8d31117aa73be14a85f8fdf3474f5463984 -x "git commit --amend --author 'Raymond <awesomecatstudio+tojavaarchive@gmail.com>' -CHEAD"
